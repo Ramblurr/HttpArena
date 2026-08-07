72e53a9da7113c114320b1bce41802fdce1b5ab9
Casey Link <casey@outskirtslabs.com>
2026-08-07 14:49:33 +0200

    pedestal: migrate database concurrency to JDBC virtual threads

--- frameworks/pedestal/src/httparena/pedestal/core.clj
+++ frameworks/pedestal/src/httparena/pedestal/core.clj
@@ -1,33 +1,35 @@
 (ns httparena.pedestal.core
   (:gen-class)
   (:require
-   [clojure.core.async :as async]
    [clojure.data.json :as json]
    [clojure.java.io :as io]
    [clojure.string :as str]
+   [hikari-cp.core :as hikari]
    [io.pedestal.connector :as conn]
    [io.pedestal.http.jetty :as jetty]
    [io.pedestal.http.route :as route]
    [io.pedestal.service.interceptors :as interceptors]
+   [next.jdbc :as jdbc]
+   [next.jdbc.result-set :as rs]
    [ring.util.response :as response])
   (:import
-   [io.vertx.core AsyncResult Handler Vertx]
-   [io.vertx.core.json JsonArray]
-   [io.vertx.pgclient PgBuilder PgConnectOptions]
-   [io.vertx.sqlclient Pool PoolOptions Row Tuple]
+   [org.eclipse.jetty.ee10.servlet ServletContextHandler]
+   [org.eclipse.jetty.server.handler.gzip GzipHandler]
+   [org.eclipse.jetty.util VirtualThreads]
+   [org.eclipse.jetty.util.thread QueuedThreadPool]
+   [org.postgresql.util PGobject]
    (java.io InputStream OutputStream)
-   (org.eclipse.jetty.ee10.servlet ServletContextHandler)
-   (org.eclipse.jetty.server.handler.gzip GzipHandler)))
+   (java.net URI)))
 
 (set! *warn-on-reflection* true)
 
 (def json-content-type "application/json")
 (def static-root "/data/static")
-(def async-db-query
+(def database-concurrency-query
   "SELECT id, name, category, price, quantity, active, tags, rating_score, rating_count
    FROM items
-   WHERE price BETWEEN $1 AND $2
-   LIMIT $3")
+   WHERE price BETWEEN ? AND ?
+   LIMIT ?")
 (def static-content-types
   {"css" "text/css"
    "js" "application/javascript"
@@ -43,8 +45,38 @@
   ([value default]
    (or (some-> value str str/trim parse-long) default)))
 
-(defn async-db-pool-size []
-  (max 1 (int (parse-long-safe (or (System/getenv "DATABASE_MAX_CONN") "256")))))
+(defn parse-int [value default]
+  (try
+    (if (some? value)
+      (Integer/parseInt (str value))
+      default)
+    (catch NumberFormatException _
+      default)))
+
+(defn database-max-conn []
+  (max 1 (parse-int (System/getenv "DATABASE_MAX_CONN") 256)))
+
+(defn database-url->hikari-options [database-url]
+  (let [uri                 (URI. database-url)
+        scheme              (.getScheme uri)
+        host                (.getHost uri)
+        port                (.getPort uri)
+        path                (.getRawPath uri)
+        query-string        (.getRawQuery uri)
+        [username password] (str/split (or (.getUserInfo uri) "") #":" 2)]
+    (when-not (and (#{"postgres" "postgresql"} scheme)
+                   (seq host)
+                   (seq path)
+                   (seq username)
+                   (some? password))
+      (throw (ex-info "invalid DATABASE_URL" {:scheme scheme})))
+    {:jdbc-url          (str "jdbc:postgresql://" host
+                            (when-not (= -1 port) (str ":" port))
+                            path
+                            (when query-string (str "?" query-string)))
+     :username          username
+     :password          password
+     :maximum-pool-size (database-max-conn)}))
 
 (defn load-dataset [path]
   (when (.exists (io/file path))
@@ -53,7 +85,7 @@
 (defonce dataset
   (delay (load-dataset "/data/dataset.json")))
 
-(defonce async-db (atom nil))
+(defonce datasource (atom nil))
 
 (defn compute-json-items [items multiplier]
   (mapv (fn [{:keys [price quantity] :as item}]
@@ -79,6 +111,10 @@
    :headers {"Content-Type" json-content-type}
    :body (json/write-str body)})
 
+(defn empty-response []
+  (json-response 200 {:items []
+                      :count 0}))
+
 (defn baseline-handler [request]
   (text-response 200 (str (request-sum request))))
 
@@ -100,68 +136,63 @@
     (text-response 200
                    (str (.transferTo stream (OutputStream/nullOutputStream))))))
 
-(defn vertx-row->item [^Row row]
-  {:id       (.getInteger row "id")
-   :name     (.getString row "name")
-   :category (.getString row "category")
-   :price    (.getInteger row "price")
-   :quantity (.getInteger row "quantity")
-   :active   (.getBoolean row "active")
-   :tags     (vec (.getList ^JsonArray (.getJsonArray row "tags")))
-   :rating   {:score (.getInteger row "rating_score")
-              :count (.getInteger row "rating_count")}})
+(defn tags->vector [tags]
+  (cond
+    (instance? PGobject tags) (json/read-str (.getValue ^PGobject tags))
+    (string? tags) (json/read-str tags)
+    (sequential? tags) (vec tags)
+    :else (throw (ex-info "unexpected tags value" {:type (type tags)}))))
 
-(defn init-async-db! []
-  (when-let [database-url (System/getenv "DATABASE_URL")]
-    (or @async-db
-        (locking async-db
-          (or @async-db
-              (try
-                (let [database (-> (PgBuilder/pool)
-                                   (.with (doto (PoolOptions.)
-                                            (.setMaxSize (async-db-pool-size))))
-                                   (.connectingTo (PgConnectOptions/fromUri database-url))
-                                   (.using (Vertx/vertx))
-                                   (.build))]
-                  (reset! async-db database))
-                (catch Throwable _
-                  nil)))))))
+(defn rows->items [rows]
+  (mapv (fn [{:keys [id name category price quantity active tags rating_score rating_count]}]
+          {:id       id
+           :name     name
+           :category category
+           :price    price
+           :quantity quantity
+           :active   active
+           :tags     (tags->vector tags)
+           :rating   {:score rating_score
+                      :count rating_count}})
+        rows))
 
-(defn async-db-handler [request]
-  (let [query-params   (:query-params request)
-        min-price      (int (parse-long-safe (or (get query-params :min) "10")))
-        max-price      (int (parse-long-safe (or (get query-params :max) "50")))
-        limit          (int (min 50 (max 1 (parse-long-safe (or (get query-params :limit) "50")))))
-        ^Pool database (init-async-db!)
-        response-ch    (async/promise-chan)
-        empty-response (json-response 200 {:items []
-                                           :count 0})
-        respond!       (fn [response]
-                         (async/put! response-ch response
-                                     (fn [_]
-                                       (async/close! response-ch))))]
-    (if database
+(defn datasource! []
+  (or @datasource
+      (locking datasource
+        (or @datasource
+            (try
+              (let [database (hikari/make-datasource
+                              (database-url->hikari-options
+                               (System/getenv "DATABASE_URL")))]
+                (reset! datasource database))
+              (catch Exception _
+                nil))))))
+
+(defn close-datasource! []
+  (when-let [database @datasource]
+    (hikari/close-datasource database)
+    (reset! datasource nil)))
+
+(defn database-concurrency-handler [request]
+  (let [query-params (into {}
+                           (map (fn [[key value]] [(name key) value])
+                                (:query-params request)))
+        min-price    (parse-int (get query-params "min") 10)
+        max-price    (parse-int (get query-params "max") 50)
+        limit        (-> (parse-int (get query-params "limit") 50)
+                         (max 1)
+                         (min 50))]
+    (if-let [database (datasource!)]
       (try
-        (-> (.preparedQuery database async-db-query)
-            (.execute (Tuple/of min-price max-price limit))
-            (.onComplete
-             (reify Handler
-               (handle [_ result]
-                 (let [^AsyncResult result result
-                       response (if (.succeeded result)
-                                  (try
-                                    (json-response 200
-                                                   (let [items (mapv vertx-row->item (.result result))]
-                                                     {:items items
-                                                      :count (count items)}))
-                                    (catch Throwable _
-                                      empty-response))
-                                  empty-response)]
-                   (respond! response))))))
-        (catch Throwable _
-          (respond! empty-response)))
-      (respond! empty-response))
-    response-ch))
+        (let [rows (jdbc/execute! database
+                                  [database-concurrency-query min-price max-price limit]
+                                  {:builder-fn rs/as-unqualified-lower-maps})
+              items (rows->items rows)]
+          (json-response 200 {:items items
+                              :count (count items)}))
+        (catch Exception _
+          (empty-response)))
+      (empty-response))))
 
 (defn static-content-type [filename]
   (let [extension (some-> filename (str/split #"\.") last str/lower-case)]
@@ -185,10 +216,14 @@
   #{["/baseline11" :get baseline-handler :route-name ::baseline-get]
     ["/baseline11" :post baseline-handler :route-name ::baseline-post]
     ["/json/:count" :get json-handler :route-name ::json]
-    ["/async-db" :get async-db-handler :route-name ::async-db]
+    ["/database-concurrency" :get database-concurrency-handler :route-name ::database-concurrency]
     ["/upload" :post upload-handler :route-name ::upload]
     ["/static/:filename" :get static-handler :route-name ::static]
     ["/pipeline" :get pipeline-handler :route-name ::pipeline]})
+
+(defn virtual-thread-pool []
+  (doto (QueuedThreadPool.)
+    (.setVirtualThreadsExecutor (VirtualThreads/getDefaultVirtualThreadsExecutor))))
 
 (defn create-connector []
   (-> (conn/default-connector-map "0.0.0.0" 8080)
@@ -197,7 +232,8 @@
       (conn/with-interceptor route/query-params)
       (conn/with-routes routes)
       (jetty/create-connector
-       {:container-options {:context-configurator (fn [^ServletContextHandler context]
+       {:container-options {:thread-pool (virtual-thread-pool)
+                            :context-configurator (fn [^ServletContextHandler context]
                                                     (let [gzip-handler (doto (GzipHandler.)
                                                                          (.addExcludedPaths
                                                                           (into-array String ["/static/*"])))]
@@ -208,4 +244,5 @@
   (when-not (vector? @dataset)
     (throw (ex-info "dataset.json must contain a JSON array"
                     {:path "/data/dataset.json"})))
+  (.addShutdownHook (Runtime/getRuntime) (Thread. ^Runnable close-datasource!))
   (conn/start! (create-connector)))
